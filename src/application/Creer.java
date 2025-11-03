9c448037952597081316081ef3e3072f4f3d60db
shirelmarciano05 <shirel.marciano05@gmail.com>
2025-11-03 23:06:31 +0100

    ajout image + css

--- src/application/Creer.java
+++ src/application/Creer.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,10 +12,12 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 
 public class Creer extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
@@ -22,13 +25,15 @@ public class Creer extends Application {
 			BorderPane root = new BorderPane();
 			
 			//entete
+			Label titre = new Label(" KAHOOT ! GO ");
+			HBox hbox = new HBox(10 , titre);
+			root.setTop(hbox);
 			
-			// titre 
-			Label label = new Label(" Creer un nouveau Kahoot!");
 
 			//vbox sur la droite
 			
 			TextField nom  = new TextField(" Nom du kahoot "); 
+			
 			Label label1 = new Label(" Type de question ");
 			
 			//liste déroulante avec bouton :
@@ -64,15 +69,30 @@ public class Creer extends Application {
 					new MenuItem(" Winter "));
 
 			Button btnSupprimer = new Button(" Supprimer ");
-			Button btnDupliquer = new Button(" Enregistrer ");
+			btnSupprimer.getStyleClass().add("btnSupprimer");
+			VBox.setMargin(btnSupprimer, new Insets(10, 20, 10, 20)); 
+			
+			Button btnEnregistrer = new Button(" Enregistrer ");
+			btnEnregistrer.getStyleClass().add("btnEnregistrer");
+			VBox.setMargin(btnEnregistrer, new Insets(10, 20, 10, 20)); 
 			
 			// on le met à la verticale 
-			VBox vbox = new VBox(10  , nom ,  label1 , m , label2 , m1 , label3 ,  m2 , label4 , m3, btnSupprimer , btnDupliquer);
+			VBox.setMargin(m, new Insets(10, 20, 10, 20)); 
+			VBox.setMargin(m1, new Insets(10, 20, 10, 20)); 
+			VBox.setMargin(m2, new Insets(10, 20, 10, 20)); 
+			VBox.setMargin(m3, new Insets(10, 20, 10, 20)); 
+			
+			VBox vbox = new VBox(15  , nom ,  label1 , m , label2 , m1 , label3 ,  m2 , label4 , m3, btnSupprimer , btnEnregistrer);
+			vbox.getStyleClass().add("vbox");
 			root.setRight(vbox);
 			
 			// au milieu 
+			
+			// titre 
+			Label label = new Label(" Creer un nouveau Kahoot!");
+						
 			TextField txt = new TextField(" Ecrit ta question ");
-			root.setLeft(txt);
+			
 			
 			GridPane grid = new GridPane();
 			
@@ -88,12 +108,14 @@ public class Creer extends Application {
 			grid.add(txt3, 1, 0);
 			grid.add(txt4, 1, 1);
 			
-			root.setCenter(grid);
+			VBox vbox2 = new VBox(10 , label , txt , grid );
+			vbox2.getStyleClass().add("vbox2");
+			root.setCenter(vbox2);
 			
-			root.setTop(label);
 			
 			Scene scene = new Scene(root,400,400);
 			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Page creer ");
 			primaryStage.setScene(scene);
 			primaryStage.show();
